a3c6dbe6ede5e6d45fd06281c51dfb3bca9f2b2c
RaulggSouza <raulggsouza@hotmail.com>
2026-04-10 00:03:40 -0300

    test: add createOrder test

---
+++ src/test/java/br/edu/ifsp/scl/ordering/application/service/order/CreateOrderServiceTest.java
@@ -0,0 +1,61 @@
+package br.edu.ifsp.scl.ordering.application.service.order;
+
+import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos.CreateOrderItemRequest;
+import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos.CreateOrderRequest;
+import br.edu.ifsp.scl.ordering.domain.entity.OrderItem;
+import br.edu.ifsp.scl.ordering.domain.valueobject.Address;
+import br.edu.ifsp.scl.ordering.domain.valueobject.ProductId;
+import br.edu.ifsp.scl.ordering.testing.tags.TDD;
+import br.edu.ifsp.scl.ordering.testing.tags.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+
+@UnitTest
+@TDD
+@ExtendWith(MockitoExtension.class)
+public class CreateOrderServiceTest {
+
+    @InjectMocks
+    CreateOrderService sut;
+
+    @Test
+    @DisplayName("shouldCreateOrder")
+    void shouldCreateOrder() {
+        List<CreateOrderItemRequest> orderItems = List.of(
+                new CreateOrderItemRequest(
+                        "12",
+                        3
+                ),
+                new CreateOrderItemRequest(
+                        "13",
+                        4
+                )
+        );
+
+        Address address = new Address(
+                "Rua A",
+                "123",
+                "São Carlos",
+                "São Paulo",
+                "456"
+        );
+
+        CreateOrderRequest body = new CreateOrderRequest(
+                "123",
+                address,
+                orderItems
+        );
+
+        UUID result = sut.create(body);
+
+        assertThat(result).isNotNull();
+    }
+}
