2941cdc7a98cdd2d9e6a164ee77c86639b493f57
RaulggSouza <raulggsouza@hotmail.com>
2026-04-11 17:13:43 -0300

    feat(order): create validation for illegal quantity on service #31

--- src/main/java/br/edu/ifsp/scl/ordering/application/service/order/CreateOrderService.java
+++ src/main/java/br/edu/ifsp/scl/ordering/application/service/order/CreateOrderService.java
@@ -37,8 +37,8 @@ public class CreateOrderService implements ICreateOrderService {
         Objects.requireNonNull(request.customerId(), "Customer must not be null");
         Objects.requireNonNull(request.items(), "OrderItems list must not be null");
         if (request.items().isEmpty()) throw new EmptyOrderItemListException("Order items list must not be empty");
-
         List<OrderItem> items = getOrderItems(request);
+
         customerRepository.findById(request.customerId())
                 .orElseThrow(() -> new CustomerNotFoundException("Customer not found. Id: "+request.customerId()));
         boolean allExistsByIds = productRepository.allExistsByIds(items.stream()
@@ -57,7 +57,10 @@ public class CreateOrderService implements ICreateOrderService {
     }
 
     private static List<OrderItem> getOrderItems(CreateOrderRequest request) {
-        request.items().forEach(item -> Objects.requireNonNull(item, "OrderItems list item must not be null"));
+        request.items().forEach(item -> {
+            Objects.requireNonNull(item, "OrderItems list item must not be null");
+            if(item.quantity() < 1) throw new IllegalArgumentException("Quantity of an Item mus not be less than one");
+        });
 
         return request.items().stream()
                 .map(item -> new OrderItem(
