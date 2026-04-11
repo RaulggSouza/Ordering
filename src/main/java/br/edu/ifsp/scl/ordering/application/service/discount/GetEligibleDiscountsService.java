30b445f65d5dca0fef1559498230c994fa861948
ArthurMascaro <arthurdevicentemascaro31@gmail.com>
2026-04-11 19:16:19 -0300

    feat(discount): create all folders, classes and methods structures, including the getEligibleDiscounts method

---
+++ src/main/java/br/edu/ifsp/scl/ordering/application/service/discount/GetEligibleDiscountsService.java
@@ -0,0 +1,28 @@
+package br.edu.ifsp.scl.ordering.application.service.discount;
+
+import br.edu.ifsp.scl.ordering.application.ports.inbound.service.discount.get_eligible_discounts.IGetEligibleDiscountsService;
+import br.edu.ifsp.scl.ordering.application.ports.inbound.service.discount.get_eligible_discounts.dtos.GetEligibleDiscountsRequest;
+import br.edu.ifsp.scl.ordering.application.ports.inbound.service.discount.get_eligible_discounts.dtos.GetEligibleDiscountsResponse;
+import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.discount.IDiscountRepository;
+import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.order.IOrderRepository;
+import br.edu.ifsp.scl.ordering.domain.aggregate.Order;
+import br.edu.ifsp.scl.ordering.domain.entity.Discount;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GetEligibleDiscountsService implements IGetEligibleDiscountsService {
+    IOrderRepository orderRepository;
+    IDiscountRepository discountRepository;
+
+    @Override
+    public GetEligibleDiscountsResponse getEligibleDiscounts(GetEligibleDiscountsRequest request) {
+        Optional<Order> order = orderRepository.findById(request.orderId());
+
+        List<Discount> discounts  = discountRepository.getAll();
+
+        return new GetEligibleDiscountsResponse(discounts);
+    }
+}
