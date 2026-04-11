eb47753b57dd8480e666e7ee913da04383da63de
RaulggSouza <raulggsouza@hotmail.com>
2026-04-10 21:47:30 -0300

    refactor(order): change customerId type to CustomerId in CreateOrderRequest #21

--- src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderRequest.java
+++ src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderRequest.java
@@ -1,8 +1,9 @@
 package br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos;
 
 import br.edu.ifsp.scl.ordering.domain.valueobject.Address;
+import br.edu.ifsp.scl.ordering.domain.valueobject.CustomerId;
 
 import java.util.List;
 
-public record CreateOrderRequest(String customerId, Address address, List<CreateOrderItemRequest> items) {
+public record CreateOrderRequest(CustomerId customerId, Address address, List<CreateOrderItemRequest> items) {
 }
