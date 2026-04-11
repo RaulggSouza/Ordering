f38c023924704181b7ee3f6d6bc83f5fef00525c
RaulggSouza <raulggsouza@hotmail.com>
2026-04-10 21:44:58 -0300

    feat(order): create customer and customer repository

--- src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderRequest.java
+++ src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderRequest.java
@@ -1,6 +1,5 @@
 package br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos;
 
-import br.edu.ifsp.scl.ordering.domain.entity.OrderItem;
 import br.edu.ifsp.scl.ordering.domain.valueobject.Address;
 
 import java.util.List;
