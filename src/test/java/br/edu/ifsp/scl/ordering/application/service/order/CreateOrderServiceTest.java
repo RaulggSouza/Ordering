9ed1be24c036e4eb3d508513aa51da5370463f51
RaulggSouza <raulggsouza@hotmail.com>
2026-04-10 23:35:17 -0300

    refactor(order): extract CreateOrderRequest creation to a private method #21

--- src/test/java/br/edu/ifsp/scl/ordering/application/service/order/CreateOrderServiceTest.java
+++ src/test/java/br/edu/ifsp/scl/ordering/application/service/order/CreateOrderServiceTest.java
@@ -1,12 +1,10 @@
 package br.edu.ifsp.scl.ordering.application.service.order;
 
-import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.ICreateOrderService;
 import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos.CreateOrderItemRequest;
 import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.dtos.CreateOrderRequest;
 import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.customer.ICustomerRepository;
 import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.product.IProductRepository;
 import br.edu.ifsp.scl.ordering.domain.aggregate.Customer;
-import br.edu.ifsp.scl.ordering.domain.entity.OrderItem;
 import br.edu.ifsp.scl.ordering.domain.valueobject.Address;
 import br.edu.ifsp.scl.ordering.domain.valueobject.CustomerId;
 import br.edu.ifsp.scl.ordering.domain.valueobject.ProductId;
@@ -15,7 +13,6 @@ import br.edu.ifsp.scl.ordering.testing.tags.UnitTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -44,6 +41,20 @@ public class CreateOrderServiceTest {
     @Test
     @DisplayName("shouldCreateOrder")
     void shouldCreateOrder() {
+        CreateOrderRequest body = createOrderRequest();
+        List<ProductId> products = body.items().stream().map(CreateOrderItemRequest::productId).toList();
+        Customer customer = new Customer(body.customerId(), "Peri");
+
+        when(customerRepository.findById(body.customerId())).thenReturn(Optional.of(customer));
+        when(productRepository.allExistsByIds(products)).thenReturn(true);
+        UUID result = sut.create(body);
+
+        assertThat(result).isNotNull();
+        verify(customerRepository, times(1)).findById(body.customerId());
+        verify(productRepository, times(1)).allExistsByIds(products);
+    }
+
+    private static CreateOrderRequest createOrderRequest() {
         List<CreateOrderItemRequest> orderItems = List.of(
                 new CreateOrderItemRequest(
                         new ProductId("12"),
@@ -61,20 +72,10 @@ public class CreateOrderServiceTest {
                 "São Paulo",
                 "456"
         );
-        CreateOrderRequest body = new CreateOrderRequest(
+        return new CreateOrderRequest(
                 new CustomerId("123"),
                 address,
                 orderItems
         );
-        List<ProductId> products = body.items().stream().map(CreateOrderItemRequest::productId).toList();
-        Customer customer = new Customer(body.customerId(), "Peri");
-
-        when(customerRepository.findById(body.customerId())).thenReturn(Optional.of(customer));
-        when(productRepository.allExistsByIds(products)).thenReturn(true);
-        UUID result = sut.create(body);
-
-        assertThat(result).isNotNull();
-        verify(customerRepository, times(1)).findById(body.customerId());
-        verify(productRepository, times(1)).allExistsByIds(products);
     }
 }
