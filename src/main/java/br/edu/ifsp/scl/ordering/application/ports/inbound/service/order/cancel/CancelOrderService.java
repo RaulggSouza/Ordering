680e99a214446b329bfdac9b39720bfcbf8e6101
RaulggSouza <raulggsouza@hotmail.com>
2026-04-12 11:32:42 -0300

    init(order): initialize cancel order issue #47

---
+++ src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/cancel/CancelOrderService.java
@@ -0,0 +1,4 @@
+package br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.cancel;
+
+public class CancelOrderService {
+}
---
+++ src/test/java/br/edu/ifsp/scl/ordering/application/service/order/CancelOrderServiceTest.java
@@ -0,0 +1,4 @@
+package br.edu.ifsp.scl.ordering.application.service.order;
+
+public class CancelOrderServiceTest {
+}
