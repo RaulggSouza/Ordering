fcce525bf2203461108457d9acc8294d15621446
RaulggSouza <raulggsouza@hotmail.com>
2026-04-12 00:38:50 -0300

    feat(order): create product repository implementations #19

---
+++ src/main/java/br/edu/ifsp/scl/ordering/infra/persistence/postgresql/product/ProductPostgresqlRepository.java
@@ -0,0 +1,27 @@
+package br.edu.ifsp.scl.ordering.infra.persistence.postgresql.product;
+
+import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.product.IProductRepository;
+import br.edu.ifsp.scl.ordering.domain.valueobject.ProductId;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductPostgresqlRepository implements IProductRepository {
+    private final SpringDataProductRepository repository;
+
+    public ProductPostgresqlRepository(SpringDataProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean allExistsByIds(List<ProductId> productIds) {
+        List<String> ids = productIds.stream()
+                .map(ProductId::value)
+                .distinct()
+                .toList();
+
+        long count = repository.countByIdIn(ids);
+        return count == ids.size();
+    }
+}
