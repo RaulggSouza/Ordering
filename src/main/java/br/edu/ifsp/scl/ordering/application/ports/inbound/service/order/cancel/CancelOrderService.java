c48ecafdf20b73958c2413de75bd46b864e748e5
RaulggSouza <raulggsouza@hotmail.com>
2026-04-12 18:15:36 -0300

    feat(order): create null id validation #57

--- src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/cancel/CancelOrderService.java
+++ src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/cancel/CancelOrderService.java
@@ -5,6 +5,8 @@ import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.order.IOr
 import br.edu.ifsp.scl.ordering.domain.aggregate.Order;
 import br.edu.ifsp.scl.ordering.domain.exceptions.OrderNotFoundException;
 
+import java.util.Objects;
+
 public class CancelOrderService {
     private final IOrderRepository orderRepository;
 
@@ -13,6 +15,7 @@ public class CancelOrderService {
     }
 
     public boolean cancel(CancelOrderRequest request){
+        Objects.requireNonNull(request.orderId());
         Order order = orderRepository.findById(request.orderId())
                 .orElseThrow(() -> new OrderNotFoundException("Order with this id not found. Id: "+request.orderId()));
 
