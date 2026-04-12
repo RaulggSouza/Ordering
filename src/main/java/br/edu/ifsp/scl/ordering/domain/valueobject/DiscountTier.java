6d8c15d6d5eff3dc71fa1c6f398e1330e3964dc1
salespace-mascaro <arthur.mascaro@salespace.com.br>
2026-04-11 21:05:55 -0300

    refactor(discount): split the discount logic into rule classes.

---
+++ src/main/java/br/edu/ifsp/scl/ordering/domain/valueobject/DiscountTier.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.ordering.domain.valueobject;
+
+public class DiscountTier {
+    private final double minimumValue;
+    private final double maximumValue;
+
+    public DiscountTier(double minimumValue, double maximumValue) {
+        this.minimumValue = minimumValue;
+        this.maximumValue = maximumValue;
+    }
+
+    public double getMinimumValue() {
+        return minimumValue;
+    }
+}
