112e32c496e81b1d5acafc94a77a2cfda72de449
ArthurMascaro <arthurdevicentemascaro31@gmail.com>
2026-04-11 22:07:40 -0300

    feat(discount): add type on discount and use it as one validator of eligibility

--- src/main/java/br/edu/ifsp/scl/ordering/domain/entity/Discount.java
+++ src/main/java/br/edu/ifsp/scl/ordering/domain/entity/Discount.java
@@ -1,23 +1,35 @@
 package br.edu.ifsp.scl.ordering.domain.entity;
 
 import br.edu.ifsp.scl.ordering.domain.aggregate.Order;
+import br.edu.ifsp.scl.ordering.domain.constant.DiscountType;
 import br.edu.ifsp.scl.ordering.domain.interfaces.DiscountRule;
 import br.edu.ifsp.scl.ordering.domain.valueobject.DiscountId;
 
 public class Discount {
     private final DiscountId discountId;
+    private final DiscountType discountType;
     private final DiscountRule rule;
 
-    public Discount(DiscountId discountId, DiscountRule rule) {
+    public Discount(DiscountId discountId, DiscountRule rule,  DiscountType discountTier) {
         this.discountId = discountId;
         this.rule = rule;
+        this.discountType = discountTier;
     }
 
     public DiscountId getDiscountId() {
         return discountId;
     }
 
+    public DiscountType getDiscountType() {
+        return discountType;
+    }
+
     public boolean isEligible(Order order) {
-        return rule.isEligible(order);
+        return rule.isEligible(order) && doesNotHaveSameTypeApplied(order);
+    }
+
+    private boolean doesNotHaveSameTypeApplied(Order order) {
+        return order.getDiscounts().stream()
+                .noneMatch(discount -> discount.getDiscountType() == this.discountType);
     }
 }
