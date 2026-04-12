20045348ad9f765b6b415aa36890690a977cf175
RaulggSouza <raulggsouza@hotmail.com>
2026-04-12 11:14:34 -0300

    - closes #19

--- src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderItemRequest.java
+++ src/main/java/br/edu/ifsp/scl/ordering/application/ports/inbound/service/order/create/dtos/CreateOrderItemRequest.java
@@ -3,8 +3,6 @@ package br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.create.
 import br.edu.ifsp.scl.ordering.domain.entity.OrderItem;
 import br.edu.ifsp.scl.ordering.domain.valueobject.ProductId;
 
-import java.util.Objects;
-
 public record CreateOrderItemRequest(ProductId productId, int quantity) {
 
     public OrderItem toDomain(){
