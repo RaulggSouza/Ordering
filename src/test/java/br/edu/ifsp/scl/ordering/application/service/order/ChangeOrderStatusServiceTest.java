59e963280e69bfa1c4972cd7070e464f5e89eda8
Gustavo Trizotti <gustavo.trizotti@aluno.ifsp.edu.br>
2026-04-15 22:50:45 -0300

    test(order): add test to verify exception thrown when invoicing a cancelled order #70

--- src/test/java/br/edu/ifsp/scl/ordering/application/service/order/ChangeOrderStatusServiceTest.java
+++ src/test/java/br/edu/ifsp/scl/ordering/application/service/order/ChangeOrderStatusServiceTest.java
@@ -5,6 +5,7 @@ import br.edu.ifsp.scl.ordering.application.ports.inbound.service.order.change_s
 import br.edu.ifsp.scl.ordering.application.ports.outbound.persistence.order.IOrderRepository;
 import br.edu.ifsp.scl.ordering.domain.aggregate.Order;
 import br.edu.ifsp.scl.ordering.domain.constant.OrderStatus;
+import br.edu.ifsp.scl.ordering.domain.exceptions.IllegalOrderOperationException;
 import br.edu.ifsp.scl.ordering.domain.valueobject.OrderId;
 import br.edu.ifsp.scl.ordering.testing.tags.TDD;
 import br.edu.ifsp.scl.ordering.testing.tags.UnitTest;
@@ -19,6 +20,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,6 +52,27 @@ public class ChangeOrderStatusServiceTest {
 
         verify(orderRepository, times(1)).findById(orderId);
         verify(orderRepository, times(1)).save(order);
+    }
+
+    @TDD
+    @UnitTest
+    @Test
+    @DisplayName("#70 - Should throw IllegalOrderOperationException when trying to invoice cancelled order")
+    void shouldThrowIllegalOrderOperationExceptionWhenTryingToInvoiceCancelledOrder() {
+        OrderId orderId = new OrderId("order-1");
+        Order order = createOrderWithStatus(orderId, OrderStatus.CANCELLED);
+
+        ChangeOrderStatusRequest request = new ChangeOrderStatusRequest(orderId, OrderStatus.INVOICED);
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+
+        assertThatExceptionOfType(IllegalOrderOperationException.class)
+                .isThrownBy(() -> sut.change(request));
+
+        assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
+
+        verify(orderRepository, times(1)).findById(orderId);
+        verify(orderRepository, never()).save(any());
     }
 
     private Order createOrderWithStatus(OrderId orderId, OrderStatus status) {
