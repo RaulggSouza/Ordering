4f9d52b2a27547cec1353ac3bcb30a68c30e1dbd
RaulggSouza <raulggsouza@hotmail.com>
2026-05-13 22:38:03 -0300

    refactor: remove inutilized code

--- src/main/java/br/edu/ifsp/scl/ordering/domain/aggregate/Customer.java
+++ src/main/java/br/edu/ifsp/scl/ordering/domain/aggregate/Customer.java
@@ -2,24 +2,10 @@ package br.edu.ifsp.scl.ordering.domain.aggregate;
 
 import br.edu.ifsp.scl.ordering.domain.valueobject.CustomerId;
 
-import java.util.Objects;
-
 public class Customer {
     private final CustomerId customerId;
 
     public Customer(CustomerId customerId, String name) {
         this.customerId = customerId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(customerId, customer.customerId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(customerId);
     }
 }
